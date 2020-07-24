5c807642d8a66ec42648f2875d1cf07daa2d3bf4
Renzo Lavado Rivas <renzolavado@gmail.com>
2020-07-24 01:53:17 -0500

    refactor: second commit

--- Condicional.py
+++ Condicional.py
@@ -1,6 +1,8 @@
-edad = 19
+edad = int(input("Ingrese edad: "))
 
-if edad > 18:
-  print ("Es mayor de edad")
+if (edad > 18) and (edad < 60):
+  print("Es mayor de edad")
+elif edad > 59:
+  print("Es adulto mayor")
 else:
-  print ("Es menor de edad")
+  print("Es menor de edad")
